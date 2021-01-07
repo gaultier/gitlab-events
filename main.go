2aac766a4db62d19134d17df40108f82db1c4e40
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-01-07 17:06:49 +0100

    Loop

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type Note struct {
@@ -23,6 +24,7 @@ type Push struct {
 }
 
 type Event struct {
+	Id             int64
 	AuthorUsername string `json:"author_username"`
 	Action         string `json:"action_name"`
 	TargetTitle    string `json:"target_title"`
@@ -30,40 +32,67 @@ type Event struct {
 	Push           *Push `json:"push_data"`
 }
 
+var seenIds []int64
+
+func idSeen(id int64) bool {
+	if len(seenIds) == 0 {
+		return false
+	}
+
+	for _, seenId := range seenIds {
+		if seenId == id {
+			return true
+		}
+	}
+	return false
+
+}
+
 func main() {
 	projectId := 138
 	token := os.Getenv("GITLAB_TOKEN")
 	url := fmt.Sprintf("https://gitlab.ppro.com/api/v4/projects/%d/events?private_token=%s", projectId, token)
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	var events []Event
-	if err = json.Unmarshal(body, &events); err != nil {
-		log.Fatal(err)
-	}
 
 	GREEN := "\x1b[32m"
 	RESET := "\x1b[0m"
 	GRAY := "\x1b[38;5;250m"
 
-	for _, event := range events {
-		fmt.Printf("🧍 %s%s%s %s%s: %s\n", GREEN, event.AuthorUsername, GRAY, event.Action, RESET, event.TargetTitle)
-		if event.Note != nil {
-			resolved := ""
-			if event.Note.Resolved {
-				resolved = "👌"
-			}
-			fmt.Printf("💬 %s %s", event.Note.Body, resolved)
-		} else if event.Push != nil {
-			fmt.Printf("⬆️  🌿 %s: %s", event.Push.Ref, event.Push.CommitTitle)
+	for {
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Fatal(err)
 		}
-		fmt.Println("\n──────────────────────────")
+
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		var events []Event
+		if err = json.Unmarshal(body, &events); err != nil {
+			log.Fatal(err)
+		}
+
+		for i := len(events) - 1; i >= 0; i-- {
+			event := events[i]
+
+			if idSeen(event.Id) {
+				continue
+			}
+			seenIds = append(seenIds, event.Id)
+			fmt.Printf("🧍 %s%s%s %s%s: %s\n", GREEN, event.AuthorUsername, GRAY, event.Action, RESET, event.TargetTitle)
+			if event.Note != nil {
+				resolved := ""
+				if event.Note.Resolved {
+					resolved = "👌"
+				}
+				fmt.Printf("💬 %s %s", event.Note.Body, resolved)
+			} else if event.Push != nil {
+				fmt.Printf("⬆️  🌿 %s: %s", event.Push.Ref, event.Push.CommitTitle)
+			}
+			fmt.Println("\n──────────────────────────")
+		}
+
+		time.Sleep(1 * time.Second)
 	}
 }
